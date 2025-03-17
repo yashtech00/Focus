3c72407ee8263fcca2a011a946e21540273e9c63
yash gupta <141262414+yashtech00@users.noreply.github.com>
2025-03-17 21:15:53 +0530

    add new pages

---
+++ app/providers.tsx
@@ -0,0 +1,10 @@
+import { SessionProvider } from "next-auth/react";
+
+
+import { ReactNode } from "react";
+
+export default function Provider({ children }: { children: ReactNode }) {
+    return <SessionProvider>
+        {children}
+    </SessionProvider>
+}
