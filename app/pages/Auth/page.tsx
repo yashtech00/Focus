32b5e19a2b33c2995ca693654bd6d4602a161203
yash gupta <141262414+yashtech00@users.noreply.github.com>
2025-03-17 22:00:43 +0530

    update

--- app/pages/Auth/page.tsx
+++ app/pages/Auth/page.tsx
@@ -1,1 +1,23 @@
+import {AuthScreen} from "@/app/components/Auth/AuthScreen";
+import { useSession } from "next-auth/react";
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
+export type SignInFlow = "signIn" | "signUp";
+
+export default function AuthPage() {
+    const session = useSession();
+    const searchParams = useSearchParams();
+
+    const formType = searchParams.get("authType") as SignInFlow;
+    const router = useRouter();
+
+    useEffect(() => {
+        if (session.status === "authenticated") {
+            router.push("/dashboard")
+        }
+    },[session.status,router])
+    
+    return <AuthScreen authType={formType} />
+}
